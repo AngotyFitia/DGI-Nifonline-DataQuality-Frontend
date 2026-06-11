1c895e94adf916c851d2ecc386185ebd2b8d5060
Angoty <angotyrabarijaona@gmail.com>
2026-06-11 15:55:09 +0300

    création de la structure initiale du projet

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="bg-red-500 text-white p-10 text-center text-2xl font-bold"> Tailwind works !</div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  darkMode: false, // or 'media' or 'class'
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
